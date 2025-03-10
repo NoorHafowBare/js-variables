64bed2ce7f8626462f377c6150181049e26f1c04
noor <noorhafowbare@gmail.com>
2025-03-10 09:12:38 +0300

    initial commit

---
+++ variables/assignment-variable.js
@@ -0,0 +1,14 @@
+// 1. Create variable name (name) & store your name.
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (gender) & store your gender.
+// 4. Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally log all variables to the console.
+
+let name = "noor-isse";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "male";
+const twitterHandle = "https://twitter.com";
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
---
+++ variables/variable.js
@@ -0,0 +1,13 @@
+// variables
+// reserved keywords, variables name and type of value
+// let name;
+// console.log(name);
+
+// assign values
+let banana;
+banana = "yellow";
+console.log(banana);
+
+const myName = "noor";
+
+console.log(myName);
